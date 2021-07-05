c8819358afa3a228ff0c365e37e0b0b28991b0fc
thinhnd <thinh6699@gmail.com>
2021-07-05 17:16:36 +0700

    create base project

--- cinema/src/views/Todo.vue
+++ cinema/src/views/Todo.vue
@@ -1,10 +1,10 @@
 <template>
-  <router-view />
+  <div>Todo</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
-export default class App extends Vue {}
+export default class Todo extends Vue {}
 </script>
