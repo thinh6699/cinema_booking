29e3f19994a0d8d2e5476a693f4076623ab8544e
thinhnd <thinh6699@gmail.com>
2021-07-14 00:48:18 +0700

    create Banner and ChangeTheme components

---
+++ cinema/src/components/Banner.vue
@@ -0,0 +1,10 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class Banner extends Vue {}
+</script>
---
+++ cinema/src/components/ChangeTheme.vue
@@ -0,0 +1,8 @@
+<template></template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class ChangeTheme extends Vue {}
+</script>
