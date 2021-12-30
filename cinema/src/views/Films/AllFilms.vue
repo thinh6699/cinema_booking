e8c4526a005b3f64adcdb86ab564443c86142268
thinhnd <thinh6699@gmail.com>
2021-12-30 17:29:00 +0700

    codding movie screen

--- cinema/src/views/Films/AllFilms.vue
+++ cinema/src/views/Films/AllFilms.vue
@@ -8,57 +8,142 @@
         Safe, secure, reliable ticketing.Your ticket to live entertainment!
       </p>
     </Banner>
-    <div class="container">
-      <p class="mb-0">1ab</p>
-      <p class="mb-0">2ab</p>
-      <p class="mb-0">3ab</p>
-      <p class="mb-0">4ab</p>
-      <p class="mb-0">5ab</p>
-      <p class="mb-0">6ab</p>
-      <p class="mb-0">7ab</p>
-      <p class="mb-0">8ab</p>
-      <p class="mb-0">9ab</p>
-      <p class="mb-0">10ab</p>
-      <p class="mb-0">11ab</p>
-      <p class="mb-0">12ab</p>
-      <p class="mb-0">13ab</p>
-      <p class="mb-0">14ab</p>
-      <p class="mb-0">15ab</p>
-      <p class="mb-0">16ab</p>
-      <p class="mb-0">17ab</p>
-      <p class="mb-0">18ab</p>
-      <p class="mb-0">19ab</p>
-      <p class="mb-0">20ab</p>
-      <p class="mb-0">21ab</p>
-      <p class="mb-0">22ab</p>
-      <p class="mb-0">23ab</p>
-      <p class="mb-0">24ab</p>
-      <p class="mb-0">25ab</p>
-      <p class="mb-0">26ab</p>
-      <p class="mb-0">27ab</p>
-      <p class="mb-0">28ab</p>
-      <p class="mb-0">29ab</p>
-      <p class="mb-0">30ab</p>
-      <p class="mb-0">31ab</p>
-      <p class="mb-0">32ab</p>
-      <p class="mb-0">33ab</p>
-      <p class="mb-0">34ab</p>
-      <p class="mb-0">35ab</p>
-      <p class="mb-0">36ab</p>
-      <p class="mb-0">37ab</p>
-      <p class="mb-0">38ab</p>
-      <p class="mb-0">39ab</p>
-      <p class="mb-0">40ab</p>
-      <p class="mb-0">41ab</p>
-      <p class="mb-0">42ab</p>
-      <p class="mb-0">43ab</p>
-      <p class="mb-0">44ab</p>
-      <p class="mb-0">45ab</p>
-      <p class="mb-0">46ab</p>
-      <p class="mb-0">47ab</p>
-      <p class="mb-0">48ab</p>
-      <p class="mb-0">49ab</p>
-      <p class="mb-0">50ab</p>
+
+    <!-- Search Section -->
+    <div class="pt-20 mb-20">
+      <div class="container">
+        <div
+          class="d-flex flex-column justify-content-center search-container p-7"
+        >
+          <div class="row align-items-center position-relative">
+            <!-- Title and introduction -->
+            <div class="text-center col-lg-4">
+              <h5 class="text-success">WELCOME TO THINH6699</h5>
+              <p class="fwb text-white fs-22">
+                WHAT MOVIES ARE YOU LOOKING FOR?
+              </p>
+            </div>
+
+            <div class="col-lg-8">
+              <div class="search-input-condition d-md-flex position-relative">
+                <!-- Search by input -->
+                <div class="position-relative mb-3 me-md-5 flex-1">
+                  <input
+                    class="search-input px-2 outline-0"
+                    type="text"
+                    placeholder="Search for Movies"
+                    v-model="movieSearch"
+                    @keyup.enter="searchMovies"
+                  />
+                  <i
+                    class="far fa-search position-absolute end-0 top-50 translate-middle-y p-3 cursor-pointer"
+                    @click="searchData"
+                  ></i>
+                </div>
+
+                <!-- Search by date -->
+                <div class="px-2 d-flex align-items-center mb-3">
+                  <img
+                    src="@/assets/images/date-condition.png"
+                    alt="date-condition"
+                    class="me-3"
+                  />
+                  <span class="text-success me-3">Date</span>
+                  <div class="date-picker">
+                    <date-picker
+                      id="date_search"
+                      v-model="dateSearch"
+                      placeholder="DD/MM/YYYY"
+                      type="date"
+                      format="DD/MM/YYYY"
+                      input-class="rounded-pill mx-input text-center bg-transparent text-white border-light-shade"
+                      class="w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- List Movies Section -->
+    <div class="list-movies mb-20">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-lg-3">
+            <div class="movies-sidebar">
+              <figure class="d-block">
+                <img src="@/assets/images/advertising1.jpg" class="img-cover" />
+              </figure>
+
+              <!-- Fiter by -->
+              <div class="filter-by">
+                <div
+                  class="d-flex justify-content-between align-items-center mb-4"
+                >
+                  <span class="text-white fs-20">Fiter By</span>
+                  <span class="text-danger fs-18">Clear all</span>
+                </div>
+
+                <!-- Experience -->
+                <div
+                  class="fiter-by-experience px-6 rounded bg-dark-tint-1 mb-8"
+                >
+                  <h5 class="mb-0 py-6 border-bottom text-white">EXPERIENCE</h5>
+                  <div class="py-6">
+                    <div class="d-flex align-items-center mb-2">
+                      <input
+                        type="checkbox"
+                        id="2D"
+                        class="w--4 h--4 me-2 bg-transparent"
+                      />
+                      <label for="2D" class="fs-16"> 2D </label>
+                    </div>
+                    <div class="d-flex align-items-center">
+                      <input
+                        type="checkbox"
+                        id="3D"
+                        class="w--4 h--4 me-2 bg-transparent"
+                      />
+                      <label for="3D" class="fs-16"> 3D </label>
+                    </div>
+                  </div>
+                </div>
+
+                <!--Category  -->
+                <div
+                  class="fiter-by-experience px-6 rounded bg-dark-tint-1 mb-4"
+                >
+                  <h5 class="mb-0 py-6 border-bottom text-white">CATEGORY</h5>
+                  <div class="py-6">
+                    <div class="d-flex align-items-center mb-2">
+                      <input
+                        type="checkbox"
+                        id="2D"
+                        class="w--4 h--4 me-2 bg-transparent"
+                      />
+                      <label for="2D" class="fs-16"> 2D </label>
+                    </div>
+                    <div class="d-flex align-items-center">
+                      <input
+                        type="checkbox"
+                        id="3D"
+                        class="w--4 h--4 me-2 bg-transparent"
+                      />
+                      <label for="3D" class="fs-16"> 3D </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-8 col-lg-9">
+            <div class="list-movies">list</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -66,9 +151,17 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Banner from '@/components/Banner.vue'
+import DatePicker from 'vue2-datepicker'
 
 @Component({
-  components: { Banner }
+  components: { Banner, DatePicker }
 })
-export default class AllFilms extends Vue {}
+export default class AllFilms extends Vue {
+  private dateSearch: string = ''
+  private movieSearch: string = ''
+
+  searchMovies(): void {
+    // search movies
+  }
+}
 </script>
