56b9b5bb18410f142589cab9458cefcb15330d81
thinhnd <thinh6699@gmail.com>
2022-02-06 17:52:18 +0700

    change slot to router-view

--- cinema/src/layouts/LayoutAuth.vue
+++ cinema/src/layouts/LayoutAuth.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <div class="py-20">
           <div class="auth-area w-100 mw--135 px-4 py-10 mx-auto">
-            <slot />
+            <router-view></router-view>
           </div>
         </div>
       </div>
