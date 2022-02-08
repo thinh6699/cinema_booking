c355852c58015b3bfb1bb431311185ef6c0d97fa
thinhnd <thinh6699@gmail.com>
2022-02-08 16:32:41 +0700

    coding ticket plan screen

--- cinema/src/views/Booking/TicketPlan.vue
+++ cinema/src/views/Booking/TicketPlan.vue
@@ -1,10 +1,163 @@
 <template>
-  <div>TicketPlan</div>
+  <div class="ticket-plan">
+    <Banner :bg_url="$store.state.movie.movieDetail.background">
+      <div>
+        <p class="fs-30 fs-lg-40 text-white text-uppercase fwb mb-6">
+          {{ $store.state.movie.movieDetail.name }}
+        </p>
+      </div>
+    </Banner>
+
+    <!-- Change condition -->
+    <div class="py-15 bg-dark-tint-1 mb-20">
+      <div class="container">
+        <div class="d-xl-flex align-items-center justify-content-around">
+          <!-- Date choose -->
+          <div
+            class="d-flex flex-column flex-column-md-unset align-items-center justify-content-center mb-md-9 mb-xl-0"
+          >
+            <div class="d-flex align-items-center mb-9 mb-md-0 mx-md-4">
+              <img
+                src="@/assets/images/date-condition.png"
+                alt="date-condition"
+                class="me-3"
+              />
+              <span class="text-success text-uppercase me-3">Date</span>
+              <b-dropdown
+                right
+                no-caret
+                variant="light rounded-pill border shadow-none"
+                menu-class="w-100 minw-unset bg-dark-shade border mt-1 text-center"
+              >
+                <template #button-content>
+                  <div class="flex-center w--30 minh--5 position-relative px-2">
+                    <span class="pe-5 text-white">{{ dateChoose }}</span>
+                    <i
+                      class="fas fa-chevron-down fs-14 position-absolute top-50 end--0 translate-middle-y px-2 text-white"
+                    />
+                  </div>
+                </template>
+                <b-dropdown-item
+                  v-for="(date, index) in listDate"
+                  :key="index"
+                  class="fs-15"
+                  @click="changeDate(date)"
+                >
+                  <span class="d-inline-block text-white py-1">{{ date }}</span>
+                </b-dropdown-item>
+              </b-dropdown>
+            </div>
+
+            <!-- Experience choose -->
+            <div class="d-flex align-items-center mb-9 mb-md-0 mx-md-4">
+              <img
+                src="@/assets/images/exp.png"
+                alt="date-condition"
+                class="me-3"
+              />
+              <span class="text-success text-uppercase me-3">experience</span>
+              <b-dropdown
+                right
+                no-caret
+                variant="light rounded-pill border shadow-none"
+                menu-class="w-100 minw-unset bg-dark-shade border mt-1 text-center"
+              >
+                <template #button-content>
+                  <div class="flex-center w--20 minh--5 position-relative px-2">
+                    <span class="pe-5 text-white">{{ experienceChoose }}</span>
+                    <i
+                      class="fas fa-chevron-down fs-14 position-absolute top-50 end--0 translate-middle-y px-2 text-white"
+                    />
+                  </div>
+                </template>
+                <b-dropdown-item
+                  v-for="(experience, index) in listExperience"
+                  :key="index"
+                  class="fs-15"
+                  @click="changeExperience(experience)"
+                >
+                  <span class="d-inline-block text-white py-1">{{
+                    experience
+                  }}</span>
+                </b-dropdown-item>
+              </b-dropdown>
+            </div>
+          </div>
+
+          <!-- Search button -->
+          <div class="flex-center">
+            <div
+              class="btn-gradient h--14 w--45 flex-center rounded-pill cursor-pointer"
+            >
+              Search
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Ticket plan detail -->
+    <div class="container">
+      <div class="bg-dark-tint-2 mb-20">
+        <div class="d-md-flex border-bottom border-light-shade">
+          <div
+            class="cinema-name w-md--70 p-6 flex-center fs-20 border-md-end border-light-shade text-white"
+          >
+            CGV Bắc Từ Liêm
+          </div>
+          <div class="timeline flex-1 pb-6 px-6 pt-md-6">
+            <div class="row mb-n6">
+              <div
+                v-for="i in 8"
+                :key="i"
+                class="col-4 col-custom-3 col-md-3 col-lg-2 mb-6 w-xl-unset text-center"
+              >
+                <span
+                  class="text-white movie-seat d-inline-block py-3 px-6 mx-n6 mx-xl-0 cursor-pointer"
+                  >23:59</span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Banner from '@/components/Banner.vue'
+import moment from 'moment'
 
-@Component({})
-export default class TicketPlan extends Vue {}
+@Component({
+  components: {
+    Banner
+  }
+})
+export default class TicketPlan extends Vue {
+  private currentDay: number = moment.now()
+  private dateChoose: string = moment(this.currentDay).format('DD/MM/YYYY')
+  private experienceChoose: string = '2D'
+  private listDate: any = []
+  private listExperience: any = []
+
+  created(): void {
+    for (let i = 0; i < 7; i++) {
+      let item = moment(this.currentDay).add(i, 'days').format('DD/MM/YYYY')
+      this.listDate.push(item)
+    }
+    this.$store.state.movie.movieDetail.format.forEach((format: any) => {
+      this.listExperience.push(format)
+    })
+  }
+
+  changeDate(date: any): void {
+    this.dateChoose = date
+  }
+
+  changeExperience(experience: any): void {
+    this.experienceChoose = experience
+  }
+}
 </script>
